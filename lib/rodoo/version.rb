ce5783dd3049e75d3656457ce624109bcc36ea42
Rodrigo Serrano <rodrigo.serrano@dekuple.es>
2025-12-24 12:08:08 +0100

    Initial gem creation

---
+++ lib/rodoo/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rodoo
+  VERSION = "0.1.0"
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "rodoo"
+
+require "minitest/autorun"
